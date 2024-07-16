059c2c509350a83e00af89d6b1150cb39f6aa690
Geoff Greer <geoff@greer.fm>
2024-07-16 10:06:45 -0700

    Add some spaces between parts of SQL statements. (#9)
    Some SQL statements have spaces between keywords. Some don't. It's suspicious that one of the ORDER BYs without spaces is breaking. I guess newlines aren't enough. Maybe they're getting stripped?

--- pkg/mssqldb/permissions.go
+++ pkg/mssqldb/permissions.go
@@ -27,17 +27,17 @@ func (c *Client) ListServerPermissions(ctx context.Context, pager *Pager) ([]*Pe
 	args := []interface{}{offset, limit + 1}
 
 	var sb strings.Builder
-	_, _ = sb.WriteString(`SELECT
-principals.name as principal_name,
-perms.grantee_principal_id as principal_id,
-perms.state as state,
-STRING_AGG(perms.type, ',') as perms,
-principals.type as principal_type
-FROM sys.server_permissions perms
-         JOIN sys.server_principals principals ON perms.grantee_principal_id = principals.principal_id
-WHERE perms.state = 'G' OR perms.state = 'W'
-GROUP BY perms.grantee_principal_id, perms.state, principals.name, principals.type
-ORDER BY perms.grantee_principal_id ASC
+	_, _ = sb.WriteString(`SELECT 
+principals.name as principal_name, 
+perms.grantee_principal_id as principal_id, 
+perms.state as state, 
+STRING_AGG(perms.type, ',') as perms, 
+principals.type as principal_type 
+FROM sys.server_permissions perms 
+         JOIN sys.server_principals principals ON perms.grantee_principal_id = principals.principal_id 
+WHERE perms.state = 'G' OR perms.state = 'W' 
+GROUP BY perms.grantee_principal_id, perms.state, principals.name, principals.type 
+ORDER BY perms.grantee_principal_id ASC 
 OFFSET @p1 ROWS FETCH NEXT @p2 ROWS ONLY`)
 
 	rows, err := c.db.QueryxContext(ctx, sb.String(), args...)
@@ -91,11 +91,11 @@ FROM `)
 	_, _ = sb.WriteString(`.sys.database_permissions perms
          JOIN `)
 	_, _ = sb.WriteString(dbName)
-	_, _ = sb.WriteString(`.sys.database_principals AS principals
-             ON perms.grantee_principal_id = principals.principal_id
-WHERE (perms.state = 'G' OR perms.state = 'W') AND (perms.class = 0 AND perms.major_id = 0)
-GROUP BY perms.grantee_principal_id, perms.state, principals.name, principals.type
-ORDER BY perms.grantee_principal_id ASC
+	_, _ = sb.WriteString(`.sys.database_principals AS principals 
+             ON perms.grantee_principal_id = principals.principal_id 
+WHERE (perms.state = 'G' OR perms.state = 'W') AND (perms.class = 0 AND perms.major_id = 0) 
+GROUP BY perms.grantee_principal_id, perms.state, principals.name, principals.type 
+ORDER BY perms.grantee_principal_id ASC 
 OFFSET @p1 ROWS FETCH NEXT @p2 ROWS ONLY`)
 
 	rows, err := c.db.QueryxContext(ctx, sb.String(), args...)
