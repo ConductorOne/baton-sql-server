73b1c61b44cd399623c2b70b0e082e6e0bee6b84
mchavez <miguel_chavez_m@hotmail.com>
2024-07-17 00:35:59 -0600

    Adding internal tests

--- pkg/connector/internal_test.go
+++ pkg/connector/internal_test.go
@@ -10,18 +10,40 @@ import (
 )
 
 var (
-	dsn = os.Getenv("BATON_DSN")
+	dsn   = os.Getenv("BATON_DSN")
+	ctx   = context.Background()
+	pager = &mssqldb.Pager{
+		Size:  0,
+		Token: `0`,
+	}
 )
 
 func TestClientListDatabasePermissions(t *testing.T) {
-	var (
-		ctx    = context.Background()
-		dbName = "temp_db"
-		pager  = &mssqldb.Pager{
-			Size:  0,
-			Token: `{"states":null,"current_state":{"token":"1","resource_type_id":"database_user","resource_id":""}}`,
-		}
-	)
+	tests := []struct {
+		name   string
+		dbName string
+	}{
+		{
+			name:   "Checking master db",
+			dbName: "master",
+		},
+		{
+			name:   "Checking tempdb db",
+			dbName: "tempdb",
+		},
+		{
+			name:   "Checking model db",
+			dbName: "model",
+		},
+		{
+			name:   "Checking msdb db",
+			dbName: "msdb",
+		},
+		{
+			name:   "Checking tempdb db",
+			dbName: "tempdb",
+		},
+	}
 
 	if dsn == "" {
 		t.Skip()
@@ -30,22 +52,78 @@ func TestClientListDatabasePermissions(t *testing.T) {
 	cli, err := mssqldb.New(ctx, dsn)
 	assert.Nil(t, err)
 
-	for pager.Token != "" {
-		pm, token, err := cli.ListDatabasePermissions(ctx, dbName, pager)
-		assert.Nil(t, err)
-		assert.NotNil(t, pm)
-		assert.NotNil(t, token)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			pager.Token = "0"
+			for keepingPagination(pager.Token) {
+				_, token, err := cli.ListDatabasePermissions(ctx, test.dbName, pager)
+				assert.Nil(t, err)
+				pager.Token = token
+			}
+		})
 	}
 }
 
 func TestClientListServerPermissions(t *testing.T) {
-	var (
-		ctx   = context.Background()
-		pager = &mssqldb.Pager{
-			Size:  0,
-			Token: `{"states":null,"current_state":{"token":"1","resource_type_id":"server_user","resource_id":""}}`,
-		}
-	)
+	if dsn == "" {
+		t.Skip()
+	}
+
+	cli, err := mssqldb.New(ctx, dsn)
+	assert.Nil(t, err)
+
+	for keepingPagination(pager.Token) {
+		_, token, err := cli.ListServerPermissions(ctx, pager)
+		assert.Nil(t, err)
+		pager.Token = token
+	}
+}
+
+func TestClientListServerRoles(t *testing.T) {
+	if dsn == "" {
+		t.Skip()
+	}
+
+	cli, err := mssqldb.New(ctx, dsn)
+	assert.Nil(t, err)
+
+	for keepingPagination(pager.Token) {
+		_, token, err := cli.ListServerRoles(ctx, pager)
+		assert.Nil(t, err)
+		pager.Token = token
+	}
+}
+
+func keepingPagination(token string) bool {
+	return token != ""
+}
+
+func TestClientListDatabaseRoles(t *testing.T) {
+	tests := []struct {
+		name   string
+		dbName string
+	}{
+		{
+			name:   "Checking master db",
+			dbName: "master",
+		},
+		{
+			name:   "Checking tempdb db",
+			dbName: "tempdb",
+		},
+		{
+			name:   "Checking model db",
+			dbName: "model",
+		},
+		{
+			name:   "Checking msdb db",
+			dbName: "msdb",
+		},
+		{
+			name:   "Checking tempdb db",
+			dbName: "tempdb",
+		},
+	}
 
 	if dsn == "" {
 		t.Skip()
@@ -54,10 +132,93 @@ func TestClientListServerPermissions(t *testing.T) {
 	cli, err := mssqldb.New(ctx, dsn)
 	assert.Nil(t, err)
 
-	for pager.Token != "" {
-		pm, token, err := cli.ListServerPermissions(ctx, pager)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			pager.Token = "0"
+			for keepingPagination(pager.Token) {
+				_, token, err := cli.ListDatabaseRoles(ctx, test.dbName, pager)
+				assert.Nil(t, err)
+				pager.Token = token
+			}
+		})
+	}
+}
+
+func TestClientListDatabaseRolePrincipals(t *testing.T) {
+	var databaseRoleID = "16393"
+	tests := []struct {
+		name   string
+		dbName string
+	}{
+		{
+			name:   "Checking master db",
+			dbName: "master",
+		},
+		{
+			name:   "Checking tempdb db",
+			dbName: "tempdb",
+		},
+		{
+			name:   "Checking model db",
+			dbName: "model",
+		},
+		{
+			name:   "Checking msdb db",
+			dbName: "msdb",
+		},
+		{
+			name:   "Checking tempdb db",
+			dbName: "tempdb",
+		},
+	}
+
+	if dsn == "" {
+		t.Skip()
+	}
+
+	cli, err := mssqldb.New(ctx, dsn)
+	assert.Nil(t, err)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			pager.Token = "0"
+			for keepingPagination(pager.Token) {
+				_, token, err := cli.ListDatabaseRolePrincipals(ctx, test.dbName, databaseRoleID, pager)
+				assert.Nil(t, err)
+				pager.Token = token
+			}
+		})
+	}
+}
+
+func TestClientListServerRolePrincipals(t *testing.T) {
+	var serverRoleID = "2"
+	if dsn == "" {
+		t.Skip()
+	}
+
+	cli, err := mssqldb.New(ctx, dsn)
+	assert.Nil(t, err)
+
+	for keepingPagination(pager.Token) {
+		_, token, err := cli.ListServerRolePrincipals(ctx, serverRoleID, pager)
 		assert.Nil(t, err)
-		assert.NotNil(t, pm)
-		assert.NotNil(t, token)
+		pager.Token = token
+	}
+}
+
+func TestClientListDatabases(t *testing.T) {
+	if dsn == "" {
+		t.Skip()
+	}
+
+	cli, err := mssqldb.New(ctx, dsn)
+	assert.Nil(t, err)
+
+	for keepingPagination(pager.Token) {
+		models, token, err := cli.ListDatabases(ctx, pager)
+		assert.Nil(t, err)
+		assert.NotNil(t, models)
+		pager.Token = token
 	}
 }
